e014af05faf5697c4913bb4e4f3e6905a8f5b227
khateeb321 <khateeb321@gmail.com>
2018-06-11 00:16:40 +0500

    C# MVC - API Controllers

---
+++ SupportWheelOfFate.Web/Server/Controllers/BAUController.cs
@@ -0,0 +1,28 @@
+﻿using SupportWheelOfFate.Data.Repository;
+using SupportWheelOfFate.Interface.IRepository;
+using SupportWheelOfFate.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SupportWheelOfFate.Web.Server.Controllers
+{
+    public class BAUController : Controller
+    {
+		private readonly IBAUDetailsRepository _repo = new BAUDetailsRepository();
+		// GET: BAU
+		public ActionResult GetAll()
+		{
+			var result = _repo.GetAll();
+			return Json(result, JsonRequestBehavior.AllowGet);
+		}
+
+		public ActionResult Save(List<BAUDetails> bauDetails)
+		{
+			var result = _repo.Save(bauDetails);
+			return Json(result, JsonRequestBehavior.AllowGet);
+		}
+	}
+}
---
+++ SupportWheelOfFate.Web/Server/Controllers/EngineersController.cs
@@ -0,0 +1,28 @@
+﻿using SupportWheelOfFate.Data.Repository;
+using SupportWheelOfFate.Interface.IRepository;
+using SupportWheelOfFate.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SupportWheelOfFate.Web.Server.Controllers
+{
+    public class EngineersController : Controller
+    {
+		private readonly IEngineersRepository _repo = new EngineersRepository();
+		// GET: Engineers
+		public ActionResult GetAll()
+		{
+			var result = _repo.GetAll();
+			return Json(result, JsonRequestBehavior.AllowGet);
+		}
+
+		public ActionResult Save(Engineers engineer)
+		{
+			var result = _repo.Save(engineer);
+			return Json(result, JsonRequestBehavior.AllowGet);
+		}
+	}
+}
